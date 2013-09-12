4fc899151ff496b80c5a7809710c3cbcb2ed7bf9
Megha Gulati <meghagulati30@gmail.com>
2013-09-12 13:10:31 -0700

    Initial Commit

---
+++ bubbles/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bubbles
+//
+//  Created by MEGHA GULATI on 9/12/13.
+//  Copyright (c) 2013 MEGHA GULATI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ bubbles/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  bubbles
+//
+//  Created by MEGHA GULATI on 9/12/13.
+//  Copyright (c) 2013 MEGHA GULATI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
